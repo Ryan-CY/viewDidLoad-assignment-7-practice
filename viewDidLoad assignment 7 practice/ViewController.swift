c8bc75124de1c14d100c324092c04a93d583521d
Ryan CY <phillin820109@msn.com>
2022-08-19 16:04:18 +0800

    Initial Commit

---
+++ viewDidLoad assignment 7 practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewDidLoad assignment 7 practice
+//
+//  Created by Ryan Lin on 2022/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
